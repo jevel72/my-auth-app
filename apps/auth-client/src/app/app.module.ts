89e10198328063a6109d69595bae3af7c0216eb0
Ramil Kutov <moreleaks@outlook.com>
2020-10-01 15:36:16 +0500

    project structure improve

--- apps/auth-client/src/app/app.module.ts
+++ apps/auth-client/src/app/app.module.ts
@@ -1,40 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './register/register.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { LoginComponent } from './login/login.component';
-import { UsersListComponent } from './users-list/users-list.component';
 
-import { FormlyModule } from '@ngx-formly/core';
-import { FormlyMaterialModule } from '@ngx-formly/material';
-
+import { AppCoreModule } from './modules/app-core.module';
 import { AppRoutingModule } from './modules/app-routing.module';
-import { AppMaterialModule } from './modules/app-material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent,
-    NotFoundComponent,
-    LoginComponent,
-    UsersListComponent,
   ],
   imports: [
+    MatButtonModule,
     BrowserModule,
-    BrowserAnimationsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    AppMaterialModule,
+    AppCoreModule,
     AppRoutingModule,
-    FormlyModule.forRoot({ extras: { lazyRender: true } }),
-    FormlyMaterialModule,
   ],
-  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ apps/auth-client/src/app/modules/app-core.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyMaterialModule } from '@ngx-formly/material';
+
+import { NgxsModule } from '@ngxs/store';
+
+import { environment } from '../../environments/environment';
+
+import { NotFoundComponent } from '../not-found/not-found.component';
+import { UsersListComponent } from '../users-list/users-list.component';
+import { LoginComponent } from '../login/login.component';
+import { RegisterComponent } from '../register/register.component';
+
+@NgModule({
+    imports: [
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        MatTableModule,
+        MatButtonModule,
+        MatIconModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatToolbarModule,
+        FormlyModule.forRoot(),
+        FormlyMaterialModule,
+        NgxsModule.forRoot([], { developmentMode: !environment.production })
+    ],
+    declarations: [
+        NotFoundComponent,
+        UsersListComponent,
+        LoginComponent,
+        RegisterComponent
+    ]
+})
+export class AppCoreModule {}
